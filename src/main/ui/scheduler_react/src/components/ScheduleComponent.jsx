399722018df24178e35c4ccb2f713fd4dc92f489
tresuave <tsharpe91@yahoo.com>
2021-06-24 17:10:38 -0400

    successful scheduler added

--- src/main/ui/scheduler_react/src/components/ScheduleComponent.jsx
+++ src/main/ui/scheduler_react/src/components/ScheduleComponent.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import ScheduleService from '../services/ScheduleService';
-import axios from 'axios';
+
 
 class ScheduleComponent extends Component {
     constructor(props){
         super (props)
+        // make use of state
         this.state = {
            scheduleData:[]
         }
     };
 
+    // will mount Schedules from the database
 componentDidMount(){
     ScheduleService.getSchedule().then((response)=>{
         this.setState({
@@ -21,11 +23,41 @@ componentDidMount(){
     render() {
         return (
             <div>
-              <h1>Schedule</h1> 
+              <h1>Scheduler</h1> 
               <table>
-                
-
-
+                  <thead>
+                <tr>
+                    <th>Week</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th> Sunday</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.scheduleData.map(
+                            schedulePiece=>
+                            <tr key = {schedulePiece.id}>
+                                
+                                <td>{schedulePiece.week}</td>
+                                <td>{schedulePiece.monday}</td>
+                                <td>{schedulePiece.tuesday}</td>
+                                <td>{schedulePiece.wednesday}</td>
+                                <td>{schedulePiece.thursday}</td>
+                                <td>{schedulePiece.friday}</td>
+                                <td>{schedulePiece.saturday}</td>
+                                <td>{schedulePiece.sunday}</td>
+                                
+                                <button onClick={()=>{}}></button>
+            
+                            </tr>
+                        )
+                    }
+                </tbody>
                   
                   </table> 
             </div>
