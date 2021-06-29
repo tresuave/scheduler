83b08c78b1642a3dd142554563c2f96816fcffa8
tresuave <tsharpe91@yahoo.com>
2021-06-29 02:56:17 -0400

    multiple get request

--- src/main/ui/scheduler_react/src/components/Home.jsx
+++ src/main/ui/scheduler_react/src/components/Home.jsx
@@ -48,9 +48,46 @@ class Home extends Component {
 
      }   
     clicked=()=>{
-        axios.all(finalurl+chart.aquarius,finalurl+chart.aries,finalurl+chart.cancer,finalurl+chart.capricorn,finalurl+chart.gemini,finalurl+chart.leo,finalurl+chart.libra,finalurl+chart.pisces,finalurl+chart.sagittarius, finalurl+chart.scorpio+finalurl+chart.taurus+finalurl+chart.virgo)
+      const one=finalurl+chart.aquarius,
+      const two=finalurl+chart.virgo,
+      const three=finalurl+chart.aries,
+      const four=finalurl+chart.cancer,
+      const five=finalurl+chart.capricorn,
+      const six=finalurl+chart.gemini,
+      const seven=finalurl+chart.leo,
+      const eight=finalurl+chart.libra,
+      const nine=finalurl+chart.pisces,
+      const ten=finalurl+chart.sagittarius,
+      const eleven=finalurl+chart.scorpio,
+      const twelve=finalurl+chart.taurus,
+
+      const requestOne = axios.get(one)
+      const requestTwo =axios.get(two)
+      const requestThree=axios.get(three)
+      const requestFour=axios.get(four)
+      const requestFive=axios.get(five)
+      const requestSix=axios.get(six)
+      const requestSeven=axios.get(seven)
+      const requestEight=axios.get(eight)
+      const requestNine=axios.get(nine)
+      const requestTen=axios.get(ten)
+      const requestEleven=axios.get(eleven)
+      const requestTwelve=axios.get(twelve)
+
+        axios.all(requestOne,requestTwo,requestThree,requestFour,requestFive,requestSix,requestSeven,requestEight,requestNine,requestTen,requestEleven,requestTwelve)
     .then(axios.spread((...response=> {
-        
+        responseOne = response[0],
+        responseTwo = response[1],
+        responseThree = response[2],
+        responseFour = response[3],
+        responseFive = response[4],
+        responseSix = response[5],
+        responseSeven = response[6],
+        responseEight = response[7],
+        responseNine = response[8],
+        responseTen = response[9],
+        responseEleven = response[10],
+        responseTwelve = response[11]
     }
          this.setState({more: response.data}
             )
